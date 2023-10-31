6968d729db337559dfe526defddae99e70a9450e
mac <>
2023-10-31 22:15:39 +0530

    app.js file

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import store, { persistor } from './src/store';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeNavigation from './src/MainTab/HomeNavigation';
 
-
-import {
-  View,
-  Text,
-  SafeAreaView,
-  FlatList,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {Provider, useDispatch, useSelector} from 'react-redux';
-import {PersistGate} from 'redux-persist/lib/integration/react';
-import store, {persistor} from './src/store';
-// import Home from "./src/Home/Home"
-import { NavigationContainer } from "@react-navigation/native";
-import HomeNavigation from "./src/MainTab/HomeNavigation"
-
-const AppWrapper = () => (
+const AppWrapper: React.FC = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <App />
     </PersistGate>
   </Provider>
-)
-const App = () => {
+);
+
+const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <HomeNavigation />    
+      <HomeNavigation />
     </NavigationContainer>
   );
 };
+
 export default AppWrapper;
